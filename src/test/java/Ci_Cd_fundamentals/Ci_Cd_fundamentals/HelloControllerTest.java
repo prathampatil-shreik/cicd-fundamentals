cb8ae4a70774492312504ebac7407378f69c9e11
prathampatil-shreik <prathampatil@shreikglobal.com>
2026-09-03 14:36:56 +0530

    INTENTIONAL FAILURE: break test to prove pipeline catches it

--- src/test/java/Ci_Cd_fundamentals/Ci_Cd_fundamentals/HelloControllerTest.java
+++ src/test/java/Ci_Cd_fundamentals/Ci_Cd_fundamentals/HelloControllerTest.java
@@ -18,7 +18,7 @@ class HelloControllerTest {
     @Test
     void healthEndpointShouldReturnOk() {
         String response = restTemplate.getForObject("http://localhost:" + port + "/health", String.class);
-        assertThat(response).isEqualTo("OK");
+        assertThat(response).isEqualTo("WRONG RESPONSE");
     }
 
     @Test
